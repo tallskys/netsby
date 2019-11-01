b37b972a245d08afc0d34096bb2663d2a8e70d99
tallskys <tallsky89@gmail.com>
2019-11-01 13:56:10 -0400

    walkthrough setup

---
+++ src/components/layout.js
@@ -0,0 +1,37 @@
+import React from "react"
+import PropTypes from "prop-types"
+import { useStaticQuery, graphql } from "gatsby"
+
+import Header from "./header"
+
+const Layout = ({ children }) => {
+  const data = useStaticQuery(graphql`query SiteTitelQuery {
+    site {
+      siteMetadata {
+        title
+      }
+    }
+  }`)
+
+  return (
+    <>
+    <Header siteTitle={data.site.siteMetadata.title} />
+    <div
+    style={{
+      margin: `0 auto`,
+      maxWidth: 900,
+      padding: `0px 1.1rem 1.45rem`,
+      paddingTop: 0,
+    }}
+    >
+      <main>{children}</main>
+    </div>
+    </>
+  )
+}
+
+Layout.propTypes = {
+  children: PropTypes.node.isRequired,
+}
+
+export default Layout
